626e7a4719c4f350a48c580e33b2b3396aa8534d
Hokkyokukou <hokkyokukou27@gmail.com>
2024-03-14 20:23:28 +0100

    update

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/signin', (req, res) => {
         req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password
     ) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -56,7 +56,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date(),
     });
