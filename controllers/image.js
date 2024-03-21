f006e8ef82aeeb7170b48d0c6fb064020a91a220
Hokkyokukou <hokkyokukou27@gmail.com>
2024-03-21 22:51:54 +0100

    update

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,96 @@
+const { ClarifaiStub, grpc } = require('clarifai-nodejs-grpc');
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set('authorization', process.env.API_CLARIFAI);
+
+// const Clarifai = require('clarifai');
+// console.log(Clarifai)
+
+const handleApiCall = (req, res) => {
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+
+            user_app_id: {
+                user_id: process.env.USER_ID,
+                app_id: process.env.APP_ID,
+            },
+
+            model_id: 'face-detection',
+            inputs: [{ data: { image: { url: req.body.input } } }],
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log('Error: ' + err);
+                return;
+            }
+
+            if (response.status.code !== 10000) {
+                console.log(
+                    'Received failed status: ' +
+                        response.status.description +
+                        '\n' +
+                        response.status.details
+                );
+                return;
+            }
+
+            console.log('Predicted concepts, with confidence values:');
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ': ' + c.value);
+            }
+            res.json(response)
+        }
+    );
+
+    // const imageurl = req.body.input;
+    // fetch(
+    //     'https://api.clarifai.com/v2/models/' + 'face-detection' + '/outputs',
+    //     returnClarifaiRequestOptions(imageurl)
+    // )
+    //     .then((response) => response.text())
+    //     .then((result) => {
+    //         res.json(result);
+    //     })
+    //     .catch((err) => res.status(400).json('unable to work with API'));
+};
+
+// const returnClarifaiRequestOptions = (imageUrl) => {
+//     // Your PAT (Personal Access Token) can be found in the portal under Authentification
+//     const PAT = '';
+//     const USER_ID = '';
+//     const APP_ID = 'face-recognition-brain';
+//     const IMAGE_URL = imageUrl;
+
+//     const raw = JSON.stringify({
+//         user_app_id: {
+//             user_id: USER_ID,
+//             app_id: APP_ID,
+//         },
+//         inputs: [
+//             {
+//                 data: {
+//                     image: {
+//                         url: IMAGE_URL,
+//                     },
+//                 },
+//             },
+//         ],
+//     });
+
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             Authorization: 'Key ' + PAT,
+//         },
+//         body: raw,
+//     };
+
+//     return requestOptions;
+// };
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
@@ -11,5 +104,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
+    handleApiCall,
     handleImage,
 };
--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        // port: 3306,
-        user: 'postgres',
-        password: 'postgres',
-        database: 'smart-brain',
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false},
+        host: process.env.DATABASE_HOST,
+        port: 5432,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DB,
     },
 });
 
@@ -33,7 +35,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json()); //bodyparser
 
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => {
     register.handleRegister(req, res, db, bcrypt);
@@ -43,6 +47,9 @@ app.get('/profile/:id', (req, res) => {
 });
 app.put('/image', (req, res) => {
     image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+    image.handleApiCall(req, res);
 });
 
 app.listen(3000, () => {
